c24f9fa4eff2ba39f1ac28eaf0ec31ada72efa99
cipollafrancesco <francesco.cipolla@chili.com>
2022-09-13 14:28:38 +0200

    Added start script

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const Telegraf = require('telegraf');
+const Telegraf = require("telegraf");
 
-const bot = new Telegraf('insert_bot_token_here');
+const bot = new Telegraf("insert_bot_token_here");
 
 //method for invoking start command
-bot.command('start', ctx => {
-    console.log(ctx.from)
-    bot.telegram.sendMessage(ctx.chat.id, 'hello there! Welcome to my new telegram bot.', {
-    })
-})
+bot.command("start", ctx => {
+    console.log(ctx.from);
+    bot.telegram.sendMessage(ctx.chat.id, "hello there! Welcome to my new telegram bot.", {});
+});
